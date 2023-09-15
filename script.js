bd5c7dc0df137a8c10e0793eafa57d51537dcac1
david31 <david31st@gmail.com>
2023-09-15 16:06:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 500;
+
+class Game {
+    constructor() {
+        this.enemies = [];
+    }
+    update() {
+
+    }
+    draw() {
+
+    }
+    #addNewEnemy() {
+
+    }
+}
+
+class Enemy {
+    constructor() {
+
+    }
+    update() {
+
+    }
+    draw() {
+
+    }
+}
+
+function animate() {
+
+}
